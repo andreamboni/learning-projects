85936426e5102a31ec13e0db00cf2c8ece1f00a7
Andre Amboni <amboni.andre@gmail.com>
2022-07-13 21:14:27 -0300

    Iniciando tutorial de React

---
+++ react/tic-tac-toe/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class ShoppingList extends React.Component {
+
+  render() {
+    return (
+      <div className='shopping-list'>
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>Whatsapp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
