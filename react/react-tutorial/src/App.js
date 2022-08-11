e74e8cdbe1bfcb5d72ad6ee1a61d1be806c79dde
Andre Amboni <amboni.andre@gmail.com>
2022-08-10 21:06:27 -0300

    Finishing React Tutorial

--- react/react-tutorial/src/App.js
+++ react/react-tutorial/src/App.js
@@ -1,30 +1,10 @@
 import React, { Component } from "react";
+import Form from "./Form";
 import Table from "./Table";
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: "Charlie",
-        job: "Janitor",
-      },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
-      {
-        name: "André",
-        job: "Developer",
-      },
-    ],
+    characters: [],
   };
 
   removeCharacter = (index) => {
@@ -38,10 +18,17 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
+    const { characters } = this.state
+
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
